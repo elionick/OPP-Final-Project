bc037297d740f5b3bfffe2c3d909e2c1640a4300
EdwardGuen <47626002+EdwardGuen@users.noreply.github.com>
2019-04-11 23:26:17 +0200

    Class User and apiBMI

--- classUser.py
+++ classUser.py
@@ -1,3 +1,4 @@
+from apiBMI import *
 class user():
     def __init__(self, first_name, middle_names, last_name, weight, height, username, password, e_mail):
         self.firstName = first_name
@@ -8,3 +9,18 @@ class user():
         self.username = username
         self.password = password
         self.eMail = e_mail
+        self.bmi = getBMI(weight, height)
+        self.statusBMI  = getBMIstatus(weight, height)
+    
+    def setAllergies(self, *allergies):
+        for index, allergy in enumerate(allergies):
+            setattr(self, "allergy" + str(index), allergy)
+        setattr(self, "nAllergies", len(allergies))
+
+if __name__ == "__main__":
+    Eddie = user("Eddie", "Alexander", "Guenther", 70, 1.83, "Eddie", 2403, "eddie.guenther@yahoo.de")
+    print(Eddie.bmi)
+    Eddie.setAllergies("Fruits", "Milk")
+    print(Eddie.nAllergies)
+    print(Eddie.allergy1)
+    print(Eddie.statusBMI)
