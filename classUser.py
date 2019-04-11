99b5f6aa00022febbb1f034f1823e4d9cf8c32aa
EdwardGuen <47626002+EdwardGuen@users.noreply.github.com>
2019-04-11 22:21:54 +0200

    initial commit classUser

---
+++ classUser.py
@@ -0,0 +1,10 @@
+class user():
+    def __init__(self, first_name, middle_names, last_name, weight, height, username, password, e_mail):
+        self.firstName = first_name
+        self.middleNames = middle_names
+        self.lastName = last_name
+        self.weight = weight
+        self.height = height
+        self.username = username
+        self.password = password
+        self.eMail = e_mail
