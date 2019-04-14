8b99279d9acd5abb5dc52fdb5711953f6f063b70
EdwardGuen <47626002+EdwardGuen@users.noreply.github.com>
2019-04-14 21:53:39 +0200

    sign up and bug fix choice error

--- menuElements.py
+++ menuElements.py
@@ -1,9 +1,12 @@
-mainMenu = ["Create User Profile", "Recipes", "Fitness", "Shopping"]
+logIn = ["Login", "Sign Up"]
+mainMenu = ["Recipes", "Fitness", "Shopping", "Logout"]
 createUserProfil = ["What is your first name?", 
                     "What is your middle name?",
                     "What is your last name?",
                     "What is your height (in meter)?",
                     "What is your weight (in kilogram)?",
                     "What is your email?",
-                    "When is your birthday (format: dd.mm.yyyy)?"
+                    "When is your birthday (format: dd.mm.yyyy)?",
+                    "Create Username:",
+                    "Create Password:"
                    ]
