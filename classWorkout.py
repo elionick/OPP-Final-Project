e9e394b22ffa4de509431a0f4edfb2ae8de135ef
EdwardGuen <47626002+EdwardGuen@users.noreply.github.com>
2019-04-25 16:42:13 +0200

    workout class and dao created

--- classWorkout.py
+++ classWorkout.py
@@ -1,2 +1,15 @@
-class workout():
-    pass
+from workoutDao import workoutDao
+class workout(workoutDao):
+    def createNewWorkout(self):
+        self.setWorkout()    
+    
+    @classmethod
+    def createListOfWorkoutObjects(cls, user_id):
+        dao_workout_list = workoutDao.getWorkoutList(user_id)
+        workout_list = []
+        for workout in dao_workout_list:
+            workout_list.append(cls(user_id, workout['TIME'], workout['WEEKDAY']))
+        return workout_list
+
+if __name__ == "__main__":
+    w_list = workout.createListOfWorkoutObjects(24)
--- workoutDao.py
+++ workoutDao.py
@@ -1,24 +1,38 @@
 from dbFunctions import *
 
-class workoutDao:
-    def setWorkout(self, starttime, weekday, user_id):
+class workoutDao():
+    def __init__(self, user_id, start_time, week_day):
+        self.userID = user_id
+        self.startTime = start_time
+        self.weekday = week_day
+    def setWorkout(self):
         try:
             with connection.cursor() as cursor:
                 sql = "insert into WORKOUTS (TIME, WEEKDAY, FK_USER_ID) values (%s, %s, %s)"
-                cursor.execute(sql, (starttime, weekday, user_id))
+                cursor.execute(sql, (self.startTime, self.weekday, self.userID))
                 connection.commit()
         finally:
             cursor.close()    
-    def getWorkoutId(self, starttime, weekday, user_id):
+    def getWorkoutId(self):
         try:
             with connection.cursor() as cursor:
                 sql = "select WORKOUT_ID FROM WORKOUTS WHERE FK_USER_ID = %s and TIME = %s and WEEKDAY = %s"
-                cursor.execute(sql, (user_id, starttime, weekday))
+                cursor.execute(sql, (self.startTime, self.weekday, self.userID))
                 info = cursor.fetchone()
         finally:
             cursor.close()
         return int(info['WORKOUT_ID'])
-
+    
+    @staticmethod
+    def getWorkoutList(user_id):
+        try:
+            with connection.cursor() as cursor:
+                sql = "select * FROM WORKOUTS WHERE FK_USER_ID = %s"
+                cursor.execute(sql, user_id)
+                info = cursor.fetchall()
+        finally:
+            cursor.close()
+        return info
+    
 if __name__ == "__main__":
-    workoutDao.setWorkout(workoutDao, "12:30", "MON", 24)
-    print(workoutDao.getWorkoutId(workoutDao, "12:30", "MON", 24))
+    pass
