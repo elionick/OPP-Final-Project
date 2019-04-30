78466a78b68b82c081963fe5513240aef7b67afa
Elio <elio.hahn@rubigen.ch>
2019-04-30 11:39:34 +0200

    bug fixes

--- dbIngredientPrice.py
+++ dbIngredientPrice.py
@@ -2,109 +2,113 @@ import migros_prices as migros
 import coop_prices as coop
 import dbFunctions
 
-
-# List with some of the most commonly used ingredients, which we are pre-saving!
-products_Migros = ["Zwiebeln", "Knoblauch", "Karotten", "Peperoni", "Tomaten",
-                   "Kopfsalat", "Valflora Halbrahm", "M-Classic Rindshackfleisch"]
-
-
-# Loop through the products with the respective functions and save the returned lists!
-for product in products_Migros:
-
-    print(f"One moment please. Trying to get the price for {product}...")
-    scraper = migros.MigrosScraper(product)
-    scraper.load_Migros_url()
-
-    # Save the three lists
-    brand = scraper.extract_product_brand()
-    name = scraper.extract_product_name()
-    price = scraper.extract_product_price()
-
-    scraper.quit()
-
-    # Prepare the data which will be sent to the DB!
-    DBEntry = []
-    for i in range(len(brand)):
-        # Only include the most relevant entries
-        if (brand[i] == "Fresh product" and ((len(name[i])-len(product)) <= 6)) or (name[i] in product):
-
-            DBEntry.append(brand[i] + "," + name[i].replace(",", "-") + "," + price[i])
-
-    # Clean and standardize the table_names
-    if brand[0] in product:
-        tablename = name[0] + "_PRICE"
-        tablename = tablename.upper()
-
-    else:
-        tablename = product + "_PRICE"
-
-    # Check if the table is already created, and if yes, enter the values
-    if dbFunctions.checkTableExist(tablename) is True:
-        dbFunctions.addNewPrice(tablename, DBEntry)
-
-    # If the table does not exist, create one and enter the values
-    else:
-        dbFunctions.createNewPriceTable(tablename, "ENTRY_ID", "BRAND", "PRODUCT", "PRICE", "DATE")
-dbFunctions.addNewPrice(tablename, DBEntry)
+def updateMigrosPrices():
+    # List with some of the most commonly used ingredients, which we are pre-saving!
+    products_Migros = ["Zwiebeln", "Knoblauch", "Karotten", "Peperoni", "Tomaten",
+                       "Kopfsalat", "Valflora Halbrahm", "M-Classic Rindshackfleisch"]
 
 
-# Loop through the products with the respective functions and save the returned lists!
-productsCoop = ["Zwiebeln", "Knoblauch", "Karotten", "Peperoni",
-                "Kopfsalat", "Halbrahm UHT", "Rinds Hackfleisch"]
+    # Loop through the products with the respective functions and save the returned lists!
+    for product in products_Migros:
 
+        print(f"One moment please. Trying to get the price for {product}...")
+        scraper = migros.MigrosScraper(product)
+        scraper.load_Migros_url()
 
-brandsCoop = ["Naturaplan", "Bio", "Prix", "Garantie", "Naturafarm", "Emmi", "Floralp", "Zweifel"]
-for product in productsCoop:
-    scraper = coop.CoopScraper(product)
+        # Save the three lists
+        brand = scraper.extract_product_brand()
+        name = scraper.extract_product_name()
+        price = scraper.extract_product_price()
 
-    source = scraper.loadCoopWebsite()
-    # Save the three lists
-    name, weight, price = scraper.extractProductInfo(source)
+        scraper.quit()
 
-    DBEntry = []
-    brand = []
+        # Prepare the data which will be sent to the DB!
+        DBEntry = []
+        for i in range(len(brand)):
+            # Only include the most relevant entries
+            if (brand[i] == "Fresh product" and ((len(name[i])-len(product)) <= 6)) or (name[i] in product):
 
-    # Separate the brand out of the name
-    for i in range(len(name)):
-        name_splited = ""
-        for coop_brand in brandsCoop:
-            if coop_brand in name[i]:
-                name_splited = name[i].split(" ")
-        brand_string = []
-        counter = 0
-        for substring in name_splited:
-            if substring in brandsCoop:
-                brand_string.append(substring)
-                counter += 1
-        if counter == 0:
-            brand.append("Coop")
+                DBEntry.append(brand[i] + "," + name[i].replace(",", "-") + "," + price[i])
+
+        # Clean and standardize the table_names
+        if brand[0] in product:
+            tablename = name[0] + "_PRICE"
+            tablename = tablename.upper()
+
         else:
-            brand.append(" ".join(brand_string))
+            tablename = product + "_PRICE"
 
-    # Check if the brand name is in the product name
-    for i in range(len(brand)):
-        if brand[i] in name[i]:
-            name[i] = name[i].replace(brand[i], "")
+        # Check if the table is already created, and if yes, enter the values
+        if dbFunctions.checkTableExist(tablename) is True:
+            dbFunctions.addNewPrice(tablename, DBEntry)
 
-        # Only include the most relevant entries
-        if ((len(name[i])-len(product)) <= 15) or (name[i] in product):
-            DBEntry.append(brand[i] + "," + name[i] + "," + price[i])
+        # If the table does not exist, create one and enter the values
+        else:
+            dbFunctions.createNewPriceTable(tablename, "ENTRY_ID", "BRAND", "PRODUCT", "PRICE", "DATE")
+    dbFunctions.addNewPrice(tablename, DBEntry)
 
-    print(DBEntry)
-    # Clean and standardize the table_names
-    if brand[0] in product:
-        tablename = name[0] + "_PRICE_COOP"
-        tablename = tablename.upper()
+def updateCoopPrices():
+    # Loop through the products with the respective functions and save the returned lists!
+    productsCoop = ["Zwiebeln", "Knoblauch", "Karotten", "Peperoni",
+                    "Kopfsalat", "Halbrahm UHT", "Rinds Hackfleisch"]
 
-    else:
-        tablename = product + "_PRICE_COOP"
 
-    # Check if the table is already created, and if yes, enter the values
-    if dbFunctions.checkTableExist(tablename) is True:
-        dbFunctions.addNewPrice(tablename, DBEntry)
+    brandsCoop = ["Naturaplan", "Bio", "Prix", "Garantie", "Naturafarm", "Emmi", "Floralp", "Zweifel"]
+    for product in productsCoop:
+        scraper = coop.CoopScraper(product)
 
-    # If the table does not exist, create one and enter the values
-    else:
-        dbFunctions.createNewPriceTable(
-            tablename, "ENTRY_ID", "BRAND", "PRODUCT", "PRICE", "DATE")
-        dbFunctions.addNewPrice(tablename, DBEntry)
+        source = scraper.loadCoopWebsite()
+        # Save the three lists
+        name, weight, price = scraper.extractProductInfo(source)
+
+        DBEntry = []
+        brand = []
+
+        # Separate the brand out of the name
+        for i in range(len(name)):
+            name_splited = ""
+            for coop_brand in brandsCoop:
+                if coop_brand in name[i]:
+                    name_splited = name[i].split(" ")
+            brand_string = []
+            counter = 0
+            for substring in name_splited:
+                if substring in brandsCoop:
+                    brand_string.append(substring)
+                    counter += 1
+            if counter == 0:
+                brand.append("Coop")
+            else:
+                brand.append(" ".join(brand_string))
+
+        # Check if the brand name is in the product name
+        for i in range(len(brand)):
+            if brand[i] in name[i]:
+                name[i] = name[i].replace(brand[i], "")
+
+            # Only include the most relevant entries
+            if ((len(name[i])-len(product)) <= 15) or (name[i] in product):
+                DBEntry.append(brand[i] + "," + name[i] + "," + price[i])
+
+        print(DBEntry)
+        # Clean and standardize the table_names
+        if brand[0] in product:
+            tablename = name[0] + "_PRICE_COOP"
+            tablename = tablename.upper()
+
+        else:
+            tablename = product + "_PRICE_COOP"
+
+        # Check if the table is already created, and if yes, enter the values
+        if dbFunctions.checkTableExist(tablename) is True:
+            dbFunctions.addNewPrice(tablename, DBEntry)
+
+        # If the table does not exist, create one and enter the values
+        else:
+            dbFunctions.createNewPriceTable(
+                tablename, "ENTRY_ID", "BRAND", "PRODUCT", "PRICE", "DATE")
+            dbFunctions.addNewPrice(tablename, DBEntry)
+
+if __name__ == "__main__":
+    updateCoopPrices()
+    updateMigrosPrices()
